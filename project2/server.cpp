e1ed51969acb91b4e7c70899cd7ba0541a617241
cheng ma <ma3cheng2@gmail.com>
2018-02-19 14:43:53 -0800

    basis of the server

--- project2/server.cpp
+++ project2/server.cpp
@@ -3,44 +3,53 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 
+#define BUFSIZE 1024
 void doTransmission(int);
 
 int main(int argc, char *argv[]) {
     int sockfd, newsockfd, portno;
     socklen_t clilen;
     struct sockaddr_in serv_addr, cli_addr;
+    socklen_t addrlen = sizeof(cli_addr);
+    int recvlen; /* the length of the received data */
+    unsigned char buf[1024]; /* used to receive the message from the client */
 
     if (argc < 2) {
         fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
     }
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         fprintf(stderr, "ERROR opening socket\n");
+        exit(1);
     }
     memset((char *)&serv_addr, '\0', sizeof(serv_addr));
 
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(portno);
 
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        fprintf(stderr, "ERROR on binding");
+        fprintf(stderr, "ERROR on binding\n");
+        exit(1);
     }
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0) {
-        fprintf(stderr, "ERROR on accept");
+
+    for (;;) {
+        printf("waiting on port %d\n", portno);
+        recvlen = recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)&cli_addr, &addrlen);
+        printf("received %d bytes\n", recvlen);
+        if (recvlen > 0) {
+            buf[recvlen] = 0;
+            printf("received message: \"%s\"\n", buf);
+        }
     }
-    doTransmission(newsockfd);
     close(sockfd);
-    close(newsockfd);
     return 0;
 }
 
