7cba33cb7f5c7e48711c628abd42d6acfa168d91
cheng ma <ma3cheng2@gmail.com>
2018-02-19 16:47:03 -0800

    basic client and server

---
+++ project2/client.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+void udp_msg_sender(int, struct sockaddr*, char*);
+
+int main(int argc, char* argv[]) {
+    int client_fd, portno;
+    struct sockaddr_in ser_addr;
+
+    if (argc < 4) {
+        fprintf(stderr, "insufficient parameters, should be client <server_hostname> <server_portnumber> <filename>\n");
+        exit(1);
+    }
+
+    client_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_fd < 0) {
+        fprintf(stderr, "ERROR opening socket\n");
+        exit(1);
+    }
+
+    memset(&ser_addr, 0, sizeof(ser_addr));
+    portno = atoi(argv[2]);
+    ser_addr.sin_family = AF_INET;
+    ser_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    ser_addr.sin_port = htons(portno);
+
+    udp_msg_sender(client_fd, (struct sockaddr*)&ser_addr, argv[3]);
+    close(client_fd);
+    return 0;
+}
+
+void udp_msg_sender(int fd, struct sockaddr* dst, char* filename) {
+    printf("client:%s\n", filename);
+    sendto(fd, filename, strlen(filename), 0, dst, sizeof(*dst));
+    printf("%s\n", filename);
+}
+
+
